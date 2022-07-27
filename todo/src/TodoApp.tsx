0a97afeeec072e861450da7a9e33b0c89eef0715
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-07-26 17:42:09 -0700

    implement form without logic and conditionally render in at todo app homepage

--- todo/src/TodoApp.tsx
+++ todo/src/TodoApp.tsx
@@ -1,9 +1,13 @@
-import styled from "styled-components";
 import "./index.css";
+import { useState } from "react";
+import styled from "styled-components";
+import { AddTodoForm } from "./components/styled/AddTodoForm";
 
 const TodoAppWrapper = styled.div`
   min-height: 100vh;
   min-width: 100vw;
+
+  background-color: gray;
 
   display: flex;
   flex-direction: column;
@@ -12,9 +16,15 @@ const TodoAppWrapper = styled.div`
 `;
 
 export const TodoApp = () => {
+  const [showForm, setShowForm] = useState<boolean>(false);
+
+  const toggleShowForm = () => setShowForm((sf) => !sf);
+
   return (
     <TodoAppWrapper>
-      <h1>Hello World</h1>
+      <button onClick={() => toggleShowForm()}>Show Form</button>
+
+      {showForm && <AddTodoForm />}
     </TodoAppWrapper>
   );
 };
