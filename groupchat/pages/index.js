c216f7285a545c6425b52ab4040b13acf386077f
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-08-10 23:30:11 -0700

    add tailwind css to project

--- groupchat/pages/index.js
+++ groupchat/pages/index.js
@@ -2,33 +2,15 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <div className="container flex-col">
       <Head>
         <title>Group Chat </title>
         <meta name="description" content="Group chat" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hello World</h1>
+      <p>I am Ricardo</p>
+      <p>I am Tito</p>
     </div>
   );
 }
-
-// export async function getServerSideProps(props) {
-//   try {
-//     // Connect to our database.
-//     await connectMongo();
-//     const users = await User.find({});
-//     return {
-//       props: {
-//         isConnected: true,
-//         data: users,
-//       },
-//     };
-//   } catch (error) {
-//     console.log(error);
-//     return {
-//       props: { isConnected: false, data: null },
-//     };
-//   }
-// }
